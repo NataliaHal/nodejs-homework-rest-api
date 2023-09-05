fb60df25501fd6a86c4cf9afef0af8a50fdd2ac9
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-09-05 21:49:44 +0200

    fixe port

--- mongoose/index.js
+++ mongoose/index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const dbURI = process.env.MONGODB_URI;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
