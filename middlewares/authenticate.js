cc54dfe51ac172a91e22a737ffadddd3ef9ba0ac
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-09-22 11:16:24 +0200

    HttpError

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,18 +7,18 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    return next(new HttpError("Unauthorized", 401));
+    return next(HttpError(401, "Unauthorized"));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token || user.token !== token) {
-      return next(new HttpError("Unauthorized", 401));
+      return next(HttpError(401, "Unauthorized"));
     }
     req.user = user;
     next();
   } catch {
-    return next(new HttpError("Unauthorized", 401));
+    return next(HttpError(401, "Unauthorized"));
   }
 };
 
